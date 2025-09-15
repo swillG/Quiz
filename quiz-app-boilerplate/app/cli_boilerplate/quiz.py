bb4e4f0f1143faf47470f20e7326033e836af282
swillG <ikechukwuchimeziri05@gmail.com>
2025-09-15 14:40:52 +0100

    done

--- quiz-app-boilerplate/app/cli_boilerplate/quiz.py
+++ quiz-app-boilerplate/app/cli_boilerplate/quiz.py
@@ -10,12 +10,15 @@ def ask_question(q):
     - Check if correct
     """
     print(q["question"])
-    print(q["options"])
-    question = input("enter your answer")
+    for options in q["options"]:
+        print(f"{options}")
+    # print(q["options"])
+    question = input("enter your answer: ")
     if question.upper() == q["answer"]:
         print(f"you are correct. The answer is {q["answer"]}")
     else:
         print(f"you are wrong. The answer is {q["answer"]}")
+
     # pass
 
 
@@ -30,7 +33,6 @@ def run_quiz():
     - Call ask_question for each
     - Print final score
     """
-
     for question in QUESTIONS:
         ask_question(question)
 
